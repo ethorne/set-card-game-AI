c29923df2afc10534fa19dddb4b6dfe3d6b1e49d
Ishan Tiwathia <ti.siahn@gmail.com>
2016-03-06 21:12:57 -0800

    Cardtracker Comments

--- cardtracker.py
+++ cardtracker.py
@@ -1,10 +1,9 @@
-# Standard imports
 import cv2
 import numpy as np
 import imutils
 import matplotlib.pyplot as plt
 
-cap = cv2.VideoCapture(1);
+cap = cv2.VideoCapture(0);
 def mse(imageA, imageB):
     # the 'Mean Squared Error' between the two images is the
     # sum of the squared difference between the two images;
@@ -17,59 +16,78 @@ def mse(imageA, imageB):
     return err
 
 def compare_images(imageA, imageB):
-    m = mse(imageA, imageB)  # MSE Increase  = less similar
-    if m < 1000 :    return False
-    else        :    return True
+    m = mse(imageA, imageB)  # Mean Squared Error Increase  = Less similar
+    if m < 1000 :    return True
+    else        :    return False
 
 while True:
+    
+    index = 12          # Total Number of Cards we are able to detect
+    
+    # Get Image
     _, frame    = cap.read();
+
+    # Transform Image to get desired values
     hsv         = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Find Values in a certain range
     sensitivity = 85
-    lower_white = np.array([0,0,255-sensitivity])
-    upper_white = np.array([180,sensitivity,255])
+    lowerwhite  = np.array([0,0,255-sensitivity])
+    upperwhite  = np.array([180,sensitivity,255])
+
+    # Increase extracted image quality, reduce blur
     frame   = imutils.resize(frame)
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    mask = cv2.inRange(hsv, lower_white, upper_white)
-    mask = cv2.erode  (mask, None, iterations=2)
-    mask = cv2.dilate (mask, None, iterations=2)
+    mask    = cv2.inRange(hsv, lowerwhite, upperwhite)
+    mask    = cv2.erode  (mask, None, iterations=2)
+    mask    = cv2.dilate (mask, None, iterations=2)
     #cv2.imshow("Mask", mask)
 
-    #Detect Contours
+    #Find Contours
     im2, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    index = 12
+    
     for cnt in contours:
         perimeter = cv2.arcLength(cnt,True)
         approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
         x,y,w,h = cv2.boundingRect(cnt)
-
-        if len(approx)==4 and perimeter > 400 and perimeter < 800 and w > 120 and h > 180:
+        # Make sure the image is of a card, by testing :
+        # Height-Width ratio
+        # Number of contours in bounding box
+        # Total Perimeter of contours
+        if len(approx)==4 and perimeter > 400 and perimeter < 1400 and w > 120 and h > 180:
                 
+
+                # Draw rectangle + text around found card
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-                cv2.putText(frame,'Card ' + str(index) ,(x+15,y-10),1,1,(0,255,0))
-                #cv2.putText(frame,str(h),(x+50,y-10),1,1,(0,255,0))
+
+                cv2.putText(frame,'Card '       + str(index)                   ,(x+15,y-50),1,1,(0,255,0))
+                cv2.putText(frame,'Height : '   + str(h) + ', Width : '+ str(w),(x+15,y-30),1,1,(0,255,0))
+                cv2.putText(frame,'Perimeter :' + str(perimeter)               ,(x+15,y-10),1,1,(0,255,0))
+                
+                # Specify that the stuff inside the contour is what we are interested in
                 roi=frame[y:y+h,x:x+w]
                 
-                if index == 12 :
+                if index == 12 : # If this is the first card found, save it 
                     cv2.imwrite('images/extracted/'+str(index)+'.png', roi)
                     index = index - 1
                 else:
                     for i in range(1, index):
 
-                            # Compare current to previously saved images
+                            # Compare current to previously saved cards
 
                         img = cv2.imread('images/extracted/'+str(index+1)+'.png')
                         #cv2.imshow("saved" , img)
-                        
+
+                        # Make sure the height and width match for MSE Calculation
                         height, width = img.shape[:2]
                         roi = cv2.resize(roi,(width, height), interpolation = cv2.INTER_CUBIC)
                         similar = compare_images(roi,img)
-                        if similar == True:
+
+                        if similar == False:  
+                            # If we think its a new card, save it
                             cv2.imwrite('images/extracted/'+str(index)+'.png', roi)
                             index = index - 1
-                            
-                #cv2.drawContours(frame,[cnt],-1,(0,0,255),2)
-    #cv2.drawContours(frame, contours, -1, (255,0,0), 3)
-
+  
     cv2.imshow("Contours" , frame)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:  break;
