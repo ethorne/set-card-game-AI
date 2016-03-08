124d04bf97d0d670ff4a8ab0e1294a10bf8c91a2
Erik Thorne <erik.j.thorne@gmail.com>
2016-03-08 14:23:16 -0800

    using HSV for color now
    works much better

--- card.py
+++ card.py
@@ -30,7 +30,8 @@ class Card():
 		#	as well as self._singleShapeOrig
 		self.Count = self.GetCount(image)
 		self.Shape = self.GetShape()
-		self.Color, self.Shade = self.GetColorAndShade()
+		self.Color = self.GetColor()
+		self.Shade = self.GetShade()
 
 	def __repr__(self):
 		shapes = ['oval', 'diamond', 'squiggle']
@@ -52,25 +53,31 @@ class Card():
 		ret += '\nCount:\t' + str(self.Count)
 		return ret
 
-	def GetColorAndShade(self):
+	def GetColor(self):
 		# there should only be two colors
 		#	white (card background)
 		#	card color
 		numColorsOnCard = 2
 
-		# these values are determiend experimentally
-		whiteLimit = 200		# if the RGB vals are above this, color is white
-		purpleDifference = 35	# if abs(red-blue) is less than this, color is purple
+		hueMax = 179.0 # max hue value for openCv
+		# for an explanation of these fractions, refer to a HSV color wheel
+		redRange = ( (3.0/4.0)*hueMax, (1.0/4.0)*hueMax )
+		greenRange = ( (1.0/4.0)*hueMax, (1.0/2.0)*hueMax )
+		purpleRange = ( (1.0/2.0)*hueMax, (3.0/4.0)*hueMax )
+		
+		print 'redRange', redRange
+		print 'greenRange', greenRange
+		print 'purpleRange', purpleRange
 
 		# convert self._singleShapeOrig to RGB
-		imageRgb = cv2.cvtColor(self._singleShapeOrig, cv2.COLOR_BGR2RGB)
+		imageHsv = cv2.cvtColor(self._singleShapeOrig, cv2.COLOR_BGR2HSV)
 
 		# reshape matrix into list of pixels
-		imageRgb = imageRgb.reshape((imageRgb.shape[0] * imageRgb.shape[1], 3))
+		imageHsv = imageHsv.reshape((imageHsv.shape[0] * imageHsv.shape[1], 3))
 		
 		# cluster with K-means clustering
 		cluster = KMeans(n_clusters = numColorsOnCard)
-		cluster.fit(imageRgb)
+		cluster.fit(imageHsv)
 
 		# create histogram of num pixels for each color
 		(histogram, notUsed) = np.histogram(cluster.labels_, bins = numColorsOnCard)
@@ -79,52 +86,40 @@ class Card():
 		histogram = histogram.astype("float")
 		histogram /= histogram.sum()
 
-		dominantColor = None
-		shade = None
-		# find dominant color that is not white
+		# find color with highest saturation
+		highestSat = -1.0e400
+		nonWhiteColor = None
 		for (percent, color) in zip(histogram, cluster.cluster_centers_):
-			red = color[0]
-			green = color[1]
-			blue = color[2]
+			if (color[1] > highestSat):
+				highestSat = color[1]
+				nonWhiteColor = color
 
-			if (red > whiteLimit and green > whiteLimit and blue > whiteLimit):
-				# this is the white card background
-				continue
+		print '\n'
+		print 'hue:\t', color[0], '\nsat:\t', color[1], '\nval:\t', color[2]
+		print '\n'
 
-			if (green > red and green > blue):
-				dominantColor = COLOR.Green
-				shade = percent
-				break
+		hue = nonWhiteColor[0]
 
-			if (abs(red - blue) < purpleDifference):
-				dominantColor = COLOR.Purple
-				shade = percent
-				break
+		if (hue > redRange[0] or hue < redRange[1]):
+			return COLOR.Red
+		if (hue > greenRange[0] and hue < greenRange[1]):
+			return COLOR.Green
+		if (hue > purpleRange[0] and hue < purpleRange[1]):
+			return COLOR.Purple
 
-			if (red > green and red > blue):
-				dominantColor = COLOR.Red
-				shade = percent
-				break
+		return None
 
-		print ' --- dominant color percent --- ', shade	
-
-		if (shade < 0.20):
-			shade = SHADE.Outlined
-		elif (shade < 0.50):
-			shade = SHADE.Striped
-		else:
-			shade = SHADE.Solid
-
-		return dominantColor, shade
+	def GetShade(self):
+		return None;
 
 	def GetCount(self, image):
-		cny = cv2.Canny(image.copy(), 225, 250)
+		cny = cv2.Canny(image.copy(), 100, 250)
 
 		count = 0
 		savedSingleShape = False
 		contours = cv2.findContours(cny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0];		
 		for contour in contours:
-			if cv2.contourArea(contour) < 500: # this number is subject to change based on what size the standard image will be
+			if cv2.contourArea(contour) < 2500: # this number is subject to change based on what size the standard image will be
 				continue
 
 			if not savedSingleShape:
@@ -139,18 +134,6 @@ class Card():
 				savedSingleShape = True
 
 			count += 1
-
-
-		# cny = cv2.Canny(image.copy(), 225, 250)
-
-		# cv2.namedWindow('1', cv2.WINDOW_NORMAL)
-		# cv2.imshow('1', self._singleShapeMod)
-		# cv2.namedWindow('2', cv2.WINDOW_NORMAL)
-		# cv2.imshow('2', self._singleShapeOrig)
-		# cv2.moveWindow('1', 0, 0)
-		# cv2.moveWindow('2', 200, 0)
-		# k = cv2.waitKey(0)
-		# cv2.destroyAllWindows()
 		return count
 
 	def GetShape(self):
@@ -169,30 +152,6 @@ class Card():
 				shape = i
 				mse = newMse
 		return shape
-
-	# UNUSED - remove?
-	def _preprocess(self, image):
-		# blur, contrast, denoise, and take inverse threshold
-		mod = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-		mod = cv2.GaussianBlur(mod, (5,5), 0)
-		mod = self._increaseContrast(mod)
-
-		mod = cv2.fastNlMeansDenoising(mod, None, 10, 21, 20)
-		mod = cv2.threshold(mod, 60, 255, cv2.THRESH_BINARY_INV)[1]
-				
-		return mod
-
-	# UNUSED - remove?
-	def _increaseContrast(self, image):
-		hist,bins = np.histogram(image.flatten(),256,[0,256])
-		cdf = hist.cumsum()
-		cdf_normalized = cdf * hist.max()/ cdf.max()
-		
-		cdf_m = np.ma.masked_equal(cdf,0)
-		cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
-		cdf = np.ma.filled(cdf_m,0).astype('uint8')
-
-		return cdf[image]
 
 	def _mse(self, imageA, imageB):
 		err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
@@ -234,11 +193,6 @@ class Card():
 		heightRight = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
 		height = max(int(heightLeft), int(heightRight))
 	 
-		# now that we have the dimensions of the new image, construct
-		# the set of destination points to obtain a "birds eye view",
-		# (i.e. top-down view) of the image, again specifying points
-		# in the top-left, top-right, bottom-right, and bottom-left
-		# order
 		dst = np.array([
 			[0, 0],
 			[width - 1, 0],
